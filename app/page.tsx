df06096b8b15012cf5485f2dd625cb00b536691d
Penguin Studios <89282211+part-avocado@users.noreply.github.com>
2025-04-04 14:23:02 -0400

    update speed insights

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import { getHouseData } from './utils/googleSheets';
 import HousePoints from './components/HousePoints';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const revalidate = 0; // Disable page caching
 
 export default async function Home() {
   const initialData = await getHouseData();
   
-  return <HousePoints initialData={initialData} />;
+  return (
+    <>
+      <HousePoints initialData={initialData} />
+      <SpeedInsights />
+    </>
+  );
 }
